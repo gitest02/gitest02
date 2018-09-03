c66d5c085e4f5ecd5b99eefe8d035f4e43b93f0c
gitlabtester01 <y2mgogo@nate.com>
2018-09-03 14:35:29 +0900

    2018/09/03 유영민(소스코드정리)

--- PD_lawhelper/src/main/java/legal/lawmapper/web/InstAdmSearchController.java
+++ PD_lawhelper/src/main/java/legal/lawmapper/web/InstAdmSearchController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import legal.lawmapper.service.InstAdmSearchService;
-import legal.lawmapper.vo.FixinfoVO;
 import legal.lawmapper.vo.InstAdmSearchVO;
 
 import com.nexacro.spring.annotation.ParamDataSet;
